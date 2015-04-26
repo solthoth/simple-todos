7d9596c851f46cf06f1072b110a5e47c1eaacf7a
Carlos Cesar Barajas Jr <soldotsol@gmail.com>
2015-04-26 07:42:07 -0700

    New tasks via html form

--- simple-todos.js
+++ simple-todos.js
@@ -1,11 +1,29 @@
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
-  // counter starts at 0
   Template.body.helpers({
-    tasks: [
-      { text: "This is task 1" },
-      { text: "This is task 2" },
-      { text: "This is task 3" }
-    ]
+    tasks: function(){
+      //sort by date
+      return Tasks.find({}, {sort: {createdAd: -1}});
+    }
+  });
+
+  Template.body.events({
+    "submit .new-task": function (event) {
+      //this function is called when the new task form is submitted
+      var text = event.target.text.value;
+
+      Tasks.insert({
+        text: text,
+        createdAt: new Date() //current time
+      });
+
+      //clear form
+      event.target.text.value = "";
+
+      //prevent default form submit
+      return false;
+    }
   });
 }
 
